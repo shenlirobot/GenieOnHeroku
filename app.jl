a98f071daba82eb6d4af1159575d973031b93ae5
Shen Li <shenlirobot@gmail.com>
2024-07-10 17:37:06 -0400

    fix

--- app.jl
+++ app.jl
@@ -3,4 +3,14 @@ route("/") do
     name = "John"
     h1("Welcome to Genie $name !")
 end
-up() # start the server
+# up() # start the server
+
+
+# To run: activate the project, run Julia and in pkg:
+# $ using GenieFramework; Genie.loadapp(); up()
+#   Or $ include("app.jl"); up()
+#   (https://learn.genieframework.com/docs/examples)
+# Then, go to http://localhost:8000/
+#   (https://learn.genieframework.com/docs/guides/creating-web-pages)
+# To shut it down:
+#   $ down()
